f98cb5626ee2e2541e4d3fd6b1c4246bea4863b4
Roman DUBININ <roman.holmes@gmail.com>
2021-04-23 14:28:23 +0300

    Just a small test of Optional

--- src/test/java/com/mockservice/MockserviceApplicationTests.java
+++ src/test/java/com/mockservice/MockserviceApplicationTests.java
@@ -1,13 +1,28 @@
 package com.mockservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.Stream;
 
 @SpringBootTest
 class MockserviceApplicationTests {
 
-	@Test
-	void contextLoads() {
+	private static class TestObject {
+		String email;
+
+		public TestObject(String email) {
+			this.email = email;
+		}
+
+		public String getEmail() {
+			return email;
+		}
 	}
 
+	// just a small test of Optional
+	public static void main(String[] args) {
+		String s = Stream.of(new TestObject("a")).filter(e -> "b".equals(e.getEmail())).findFirst()
+				.map(TestObject::getEmail).orElse("c");
+		System.out.println(s);
+	}
 }
